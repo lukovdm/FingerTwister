5a1ff23e4633f222d1a70e3d19a76e57d55e6b66
luko van der maas <lukovandermaas@gmail.com>
2014-08-23 17:19:47 +0200

    added bord and spinning wheel

---
+++ Assets/scripts/Spin.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+	public int angularSpeed;
+
+	public void SpinArrow(){
+		rigidbody2D.AddTorque (angularSpeed);
+		Debug(
+	}
+}
